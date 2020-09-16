61d9c5a9483e3c369d44dedd3131c7932ad57442
Mia Wan <51465779+mia1996@users.noreply.github.com>
2020-09-15 21:11:02 -0500

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,18 @@
 class Movie < ActiveRecord::Base
+
+  # Part2 BEGIN
+  def self.get_all_ratings
+    return self.pluck(:rating).uniq
+  end
+
+  def self.with_ratings(ratings)
+    return self.where(rating: ratings)
+  end
+  # Part2 END
+
+  # Part3 BEGIN
+  def self.with_ratings_sort(ratings, sort_by)
+    return self.where(rating: ratings).order(sort_by)
+  end
+  # Part3 END
 end
